dfb8b59a3001300464fdb4b9e81ddcbf7a13b1d6
Igor Lousada <igorcoutinho97@gmail.com>
2020-10-02 11:27:56 -0300

    adicionando getters e setters

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ print(vingadores)
 print(breakingbad)
 
 vingadores.dar_like()
-print(vingadores.like)
+print(vingadores.likes)
+
+print(vingadores.likes)
+print(vingadores.nome)
+vingadores.ano = 2013
+print(vingadores.ano)
+vingadores.duracao = 200
+print(vingadores.duracao)
--- modelo.py
+++ modelo.py
@@ -1,20 +1,61 @@
 class ClasseFilme:
-   def __init__(self,nome,ano,duracao,likes=0):
+   def __init__(self,nome,ano,duracao):
        self.__nome = nome.title()
        self.__ano = ano
        self.__duracao = duracao
-       self.__likes = likes
+       self.__likes = 0
     
    def dar_like(self):
        self.__likes +=1  
    @property
-   def like(self):
+   def likes(self):
        return self.__likes
+   @property
+   def nome(self):
+       return self.__nome
+   @property
+   def ano(self):
+       return self.__ano
+   @ano.setter
+   def ano(self,valor):
+       self.__ano = valor    
+   @property
+   def duracao(self):
+       return self.__duracao
+   @duracao.setter
+   def duracao(self,valor):
+       self.__duracao = valor
+       
+
+
+
 class ClasseSerie:
 
     def __init__(self,nome,ano,temporadas):
         self.__nome = nome.title()
         self.__ano = ano
-        self.__temporadas = temporadas    
+        self.__temporadas = temporadas
+        self.__likes = 0
+    def dar_like(self):
+        self.__likes += 1
+    @property
+    def likes(self):
+        return self.__likes
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def ano(self):
+        return self.__ano
+    @property
+    def temporadas(self):
+        return self.__temporadas
+    @temporadas.setter
+    def temporadas(self,valor):
+        self.__temporadas = valor    
+
+
+
+
 
 
